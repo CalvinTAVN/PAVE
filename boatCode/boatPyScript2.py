ed8f180691f3ca37aa2299e45aa1b34fcb7fbff4
Calvin Nguyen <cn1182@princeton.edu>
2024-04-03 18:32:31 -0400

    123

--- boatCode/boatPyScript2.py
+++ boatCode/boatPyScript2.py
@@ -2,6 +2,7 @@ import time
 import serial
 import RPi.GPIO as GPIO
 from datetime import datetime
+import sys
 
 #receiving signal information from pico
 #bottom right usb port for wifi dongle
@@ -16,11 +17,15 @@ now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 file = open(current_time + ".txt", "w")
 
+def print(text):
+    file.write(str(text)+"\n")
+    sys.stdout.write(str(text)+"\n")
+
 arduinos = []
 for port, desc, hwid in sorted(ports):
     print(hwid)
     for i in hwid.split():
-        if i.startswith("SER="):
+        if i.startswith("SER="):    
             if i == "SER=e661640843856f28":
                 pico = serial.Serial(port=port, baudrate=9600, timeout=0.1)
                 print("mappedPico")
@@ -45,35 +50,41 @@ pwm.start(0)
 def write_read(controller, x= ""):
     if (x != ""):
         controller.write(bytes(x, 'utf-8'))
-    time.sleep(0.01)
+    #time.sleep(0.01)
     data = controller.readline()
     return data
 
 
 
-file.write("Start")
+print("Start")
 killCounter = 0
 counter = 0
 cycle = 0
 connectedPico = True
 lastMessage = ""
-while killCounter <= 100:
-    #print("kilCount: ", killCounter)
+while True:
     try:
         receivedSignal = write_read(pico) 
-    except:
+        connectedPico = True
+    except serial.serialutil.SerialException:
+        print("picoUnplugged")
         pwm.start(0)    
         connectedPico = False
-    if connectedPico:    
+    if connectedPico: 
         stringIn = receivedSignal.decode("utf-8").replace("\r", "").replace("\n", "")
+        #print(f"counter: {counter} {stringIn}")
         if stringIn != "" and len(stringIn) == 9 and stringIn[8]=="0":
             if stringIn != lastMessage:
                 killCounter = 0
+                counter = 0
             if cycle == 0:
                 #print(stringIn)
                 steeringInformation = stringIn[0:4]
                 throttleInfo = stringIn[5:8]
-                throttle = int(throttleInfo)
+                if (counter < 100):
+                    throttle = int(throttleInfo)
+                else:
+                    throttle = 0
                 #print("throttle: ", throttle)
                 pwm.start(throttle)
                 #print(steeringInformation)
@@ -82,26 +93,21 @@ while killCounter <= 100:
                         write_read(arduino, steeringInformation)  
                     except: 
                         pwm.start(0) 
-                file.write("steering: " + steeringInformation + " throttle: " + throttleInfo + " counter: " + str(counter))
+                #print(stringIn)
+                print("steering: " + steeringInformation + " throttle: " + str(throttle) + " counter: " + str(counter))
                 lastMessage = stringIn
-            cycle = (cycle + 1) % 10
-            counter = 0
+            #cycle = (cycle + 1) % 10
             pico.flush()
         counter+=1
         if (counter > 100 or ((len(stringIn) == 9) and (stringIn[8]=="1"))):
             #for arduino in arduinos:
                 #write_read(arduinos, "0500")
             pwm.start(0)
-            file.write("noConnection")
+            print("noConnection")
     
     else:
+        print("not connected Pico")
         pwm.start(0)
-
-    killCounter+=1
-else:
-    pwm.start(0)
-    file.write("killed")
-    file.close()
 
 
         
