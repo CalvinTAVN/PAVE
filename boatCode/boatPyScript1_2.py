2e110d6f66e536a7098a6a9d2de6ef5c15392d5e
Calvin Nguyen <cn1182@princeton.edu>
2024-04-09 20:18:17 -0400

    123

--- boatCode/boatPyScript1_2.py
+++ boatCode/boatPyScript1_2.py
@@ -88,11 +88,14 @@ while True:
                     throttle = 0
                 pwm.start(throttle)
                 for arduino in arduinos:
+                    write_read(arduino, False, steeringInformation)  
+                    """
                     try:
                         write_read(arduino, False, steeringInformation)  
                     except: 
                         print("oofCantSendArduinoSerial")
                         pwm.start(0)
+                    """
                 print("steering: " + steeringInformation + " throttle: " + str(throttle) + " counter: " + str(counter))
             cycle = (cycle + 1) % 10
             lastMessage = stringIn
